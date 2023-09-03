c8bf6467a1a918c2e456344823540038ff64561b
Jiage <liujiage@users.noreply.github.com>
2023-09-03 22:53:37 +0800

    init
    init

---
+++ app.py
@@ -0,0 +1,13 @@
+import re
+import sys
+from pyspark import SparkConf, SparkContext
+
+if __name__ == "__main__":
+    conf = SparkConf()
+    sc = SparkContext(conf=conf)
+    lines = sc.textFile(sys.argv[1])
+    words = lines.flatMap(lambda l: re.split(r'[^\w]+', l))
+    pairs = words.map(lambda w: (w, 1))
+    counts = pairs.reduceByKey(lambda n1, n2: n1 + n2)
+    counts.coalesce(1).saveAsTextFile(sys.argv[2])
+    sc.stop()
